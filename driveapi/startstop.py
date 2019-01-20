ff1c12db845600a1175875e78667b3c3ec6912c4
kuderr <dakudryavcev@gmail.com>
2019-01-20 16:25:34 +0300

    doesn`t work, but i don`t give up

--- driveapi/startstop.py
+++ driveapi/startstop.py
@@ -1,26 +1,19 @@
 import datetime
 from subprocess import Popen
 import os
-import time
 
 from driveAPI.driveSettings import upload
 
-
 def start(roomIndex):
-    rooms = {"4": "513MIEM", "1": "P500", "3": "P500", "2": "S401"}
+    rooms = {"1": "P505", "2": "P500", "3": "S401", "4": "513MIEM"}
     global process
     today = datetime.date.today()
     global record
     record = "{0}-{1}-{2}-{3}-{4}".format(
         today.year, today.month, today.day, rooms[roomIndex], "HSE") + ".mp4"
     process = Popen("ffmpeg -i rtsp://192.168.11." +
-                    roomIndex + "3 -y -c:v copy -an -f mp4 " + os.getcwd() + "\\" + record)
+                    roomIndex + "5 -y -c:v copy -an -f mp4 " + os.getcwd() + "\\" + record)
 
 def stop():
     Popen.kill(process)
     upload(record)
-
-
-start("4")
-time.sleep(5)
-stop()
