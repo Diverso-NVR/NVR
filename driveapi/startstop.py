da935da37ddacb63b39135b7e262bf5d3aaa5432
kuderr <dakudryavcev@gmail.com>
2019-02-05 12:41:02 +0300

    .

--- driveapi/startstop.py
+++ driveapi/startstop.py
@@ -21,19 +21,19 @@ def start(roomIndex):
     record = "{0}-{1}-{2}-{3}-{4}".format(
         today.year, today.month, today.day, rooms[roomIndex], "HSE") + ".mp4"
     enc = subprocess.Popen("ffmpeg -i rtsp://192.168.11." +
-                    roomIndex + "1/main -y -c copy -f mp4 ../../vids/enc"
+                    roomIndex + "1/main -y -c copy -f mp4 ~/vids/enc"
                     + record, shell=True, preexec_fn=os.setsid)
     processes.append(enc)
 
     # # cams *2 throw 401: unauthorized error in S401 and P500
     # cam = subprocess.Popen("ffmpeg -i rtsp://192.168.11." +
-    #                        roomIndex + "2 -y -c:v copy -f mp4 ../../vids/cam"
+    #                        roomIndex + "2 -y -c:v copy -f mp4 .~/vids/cam"
     #                        + record, shell=True, preexec_fn=os.setsid)
     # processes.append(cam)
 
     for i in range(3, 7):
         process = subprocess.Popen("ffmpeg -i rtsp://192.168.11." +
-                    roomIndex + str(i) + " -y -c:v copy -f mp4 ../../vids/"
+                    roomIndex + str(i) + " -y -c:v copy -f mp4 ~/vids/"
                     + str(i) + "-" + record, shell=True, preexec_fn=os.setsid)
         processes.append(process)
     # TODO: Add multiprocessing
@@ -53,8 +53,8 @@ def stop():
 
 
 def get_sound(record):
-    os.system("ffmpeg -y -i ../../vids/enc" + record + " -vn ../../vids/sound" + record + ".mp3")
+    os.system("ffmpeg -y -i ~/vids/enc" + record + " -vn ~/vids/sound" + record + ".mp3")
 
 
 def add_sound(video_cam_num, audio_cam_num):
-    os.system("ffmpeg -y -i " + video_cam_num + " -i ../../vids/sound" + audio_cam_num + ".mp3 " + video_cam_num)
+    os.system("ffmpeg -y -i " + video_cam_num + " -i ~/vids/sound" + audio_cam_num + ".mp3 " + video_cam_num)
