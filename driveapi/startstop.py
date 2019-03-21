9f9343dfe00ec204306757691bab00e51a262515
kuderr <dakudryavcev@gmail.com>
2019-03-21 17:59:22 +0300

    optimisation

--- driveapi/startstop.py
+++ driveapi/startstop.py
@@ -24,23 +24,23 @@ def start(roomIndex, soundType):
         today.year, today.month, today.day, formattedTime,rooms[roomIndex], "HSE")
     if soundType == "enc":
         enc = subprocess.Popen("ffmpeg -i rtsp://192.168.11." +
-                               roomIndex + "1/main -y -c copy -f mp4 ../vids/sound-source-"
-                               + records[roomIndex] + ".mp4", shell=True, preexec_fn=os.setsid)
+                               roomIndex + "1/main -y -c:a copy -vn -f mp4 ../vids/sound-source-"
+                               + records[roomIndex] + ".mp3", shell=True, preexec_fn=os.setsid)
         processes[roomIndex].append(enc)
     else:
         # cams *2 throw 401: unauthorized error in S401 and P500
         cam = subprocess.Popen("ffmpeg -i rtsp://192.168.11." +
-                               roomIndex + "2 -y -c:v copy -f mp4 ../vids/sound-source-"
-                               + records[roomIndex] + ".mp4", shell=True, preexec_fn=os.setsid)
+                               roomIndex + "2 -y -c:a copy -vn -f mp4 ../vids/sound-source-"
+                               + records[roomIndex] + ".mp3", shell=True, preexec_fn=os.setsid)
         processes[roomIndex].append(cam)
 
     proc = subprocess.Popen("ffmpeg -i rtsp://192.168.11." +
-                               roomIndex + "1/main -y -c:v copy -f mp4 ../vids/1-" +
+                               roomIndex + "1/main -y -c copy -f mp4 ../vids/1-" +
                             records[roomIndex] + ".mp4", shell=True, preexec_fn=os.setsid)
     processes[roomIndex].append(proc)
     for i in range(2, 7):
         process = subprocess.Popen("ffmpeg -i rtsp://192.168.11." +
-                                   roomIndex + str(i) + " -y -c:v copy -f mp4 ../vids/"
+                                   roomIndex + str(i) + " -y -c:v copy -an -f mp4 ../vids/"
                                    + str(i) + "-" + records[roomIndex] + ".mp4", shell=True, preexec_fn=os.setsid)
         processes[roomIndex].append(process)
 
@@ -49,7 +49,6 @@ def stop(roomIndex):
     try:
         for process in processes[roomIndex]:
             os.killpg(os.getpgid(process.pid), signal.SIGTERM)
-        get_sound(records[roomIndex])
         for i in range(1, 7):
             add_sound(str(i) + "-" + records[roomIndex], records[roomIndex])
         for i in range(1, 7):
@@ -58,11 +57,7 @@ def stop(roomIndex):
         pass
 
 
-def get_sound(record):
-    os.system("ffmpeg -i ../vids/sound-source-" + record + ".mp4" + " -vn ../vids/sound-" + record + ".mp3")
-
-
 def add_sound(video_cam_num, audio_cam_num):
     os.system(
-        "ffmpeg -i ../vids/sound-" + audio_cam_num + ".mp3 "
+        "ffmpeg -i ../vids/sound-source-" + audio_cam_num + ".mp3 "
         + "-i ../vids/" + video_cam_num + ".mp4" + " ../vids/result-" + video_cam_num + ".mp4")
