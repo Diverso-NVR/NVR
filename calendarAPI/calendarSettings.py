997964a23ca0a67f6b1b0085fef2e50924559394
kuder <dakudryavcev@gmail.com>
2019-04-23 12:59:42 +0300

    calendar hotfix

--- calendarAPI/calendarSettings.py
+++ calendarAPI/calendarSettings.py
@@ -24,7 +24,6 @@ def parseDate(date):
     day = date[8:10]
     hour = date[11:13]
     minute = date[14:16]
-    # second = date[17:19]
     return "{}-{}-{} {}:{}".format(year, month, day, hour, minute)
 
 
