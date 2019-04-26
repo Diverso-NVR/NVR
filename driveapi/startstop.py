3dbe1d28ea1be1dec5ac996e53a00b46a6adf5b5
kuder <dakudryavcev@gmail.com>
2019-04-26 13:48:58 +0300

    hotfix

--- driveapi/startstop.py
+++ driveapi/startstop.py
@@ -96,12 +96,12 @@ def start(room_index, sound_type, building):
                                + records[building][room_index] + ".mp3", shell=True, preexec_fn=os.setsid)
         processes[building][room_index].append(cam)
 
-    proc = subprocess.Popen("ffmpeg -i rtsp://192.168." + network[building] + "." +
+    proc = subprocess.Popen("ffmpeg -rtsp_transport udp -i rtsp://192.168." + network[building] + "." +
                             room_index + "1/main -y -c:v copy -an -f mp4 ../vids/1-" +
                             records[building][room_index] + ".mp4", shell=True, preexec_fn=os.setsid)
     processes[building][room_index].append(proc)
     for i in range(2, 7):
-        process = subprocess.Popen("ffmpeg -i rtsp://admin:Supervisor@192.168." + network[building] + "." +
+        process = subprocess.Popen("ffmpeg -rtsp_transport udp -i rtsp://admin:Supervisor@192.168." + network[building] + "." +
                                    room_index +
                                    str(i) + " -y -c:v copy -an -f mp4 ../vids/"
                                    + str(i) + "-" + records[building][room_index] + ".mp4", shell=True, preexec_fn=os.setsid)
