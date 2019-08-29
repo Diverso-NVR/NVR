ca452e7ee3a5faf1ab928b481b421d6c9a460174
Burnouttt <burnouttt@yandex.ru>
2019-08-29 14:39:45 +0300

    merge api update

--- mergeServer/run.py
+++ mergeServer/run.py
@@ -6,8 +6,13 @@ from flask import Flask, request
 app = Flask("NVR_VIDEO_MERGE")
 
 
+@app.route('/', methods=["GET", "POST"])
+def main():
+    return "Merge server v1.0", 200
+
+
 @app.route('/merge', methods=["POST"])
-def startMerge():
+def start_merge():
     json = request.get_json(force=True)
     Thread(target=merge_video,
            args=(json["screen_num"], json["video_cam_num"], json["record_num"], json["room_name"])).start()
