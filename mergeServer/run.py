8a0d06038b8c3545a10931d5a8a71f2aeb9afd68
Burnouttt <burnouttt@yandex.ru>
2019-09-06 18:16:11 +0300

    Client url update

--- mergeServer/run.py
+++ mergeServer/run.py
@@ -14,7 +14,7 @@ def main():
 @app.route('/merge', methods=["POST"])
 def start_merge():
     json = request.get_json(force=True)
-    url = request.remote_addr
+    url = "http://172.16.87.10"
     Thread(target=merge_video,
            args=(json["screen_num"], json["video_cam_num"], json["record_num"],
                  json["room_name"], url)).start()
