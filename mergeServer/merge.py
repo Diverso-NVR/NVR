377590baabee7945a78975660b4b7d9f4bb735fd
kuder <dakudryavcev@gmail.com>
2019-10-08 16:21:04 +0300

    merge type change

--- mergeServer/merge.py
+++ mergeServer/merge.py
@@ -11,37 +11,37 @@ home = str(Path.home())
 def merge_video(client_url: str, screen_num: str, cam_num: str, record_name: str,  room_id: int, calendar_id: str, event_id: str) -> None:
     lock.acquire()
 
-    # first = subprocess.Popen(["ffmpeg", "-i", home + "/vids/vid_" + cam_num + ".mp4", "-i", "../vids/vid_" +
-    #                           screen_num + ".mp4", "-filter_complex", "hstack=inputs=2", "../vids/vid_" +
-    #                           record_name + "merged.mp4"], shell=False)
-    # os.system("renice -n 20 %s" % (first.pid, ))
-    # first.wait()
+    first = subprocess.Popen(["ffmpeg", "-i", home + "/vids/vid_" + cam_num + ".mp4", "-i", "../vids/vid_" +
+                              screen_num + ".mp4", "-filter_complex", "hstack=inputs=2", "../vids/vid_" +
+                              record_name + "merged.mp4"], shell=False)
+    os.system("renice -n 20 %s" % (first.pid, ))
+    first.wait()
 
-    mid1 = subprocess.Popen(
-        ["ffmpeg", "-i", home + "/vids/vid_" + screen_num + ".mp4", "-s", "hd720",
-         home + "/vids/" + record_name + "mid_screen.mp4"], shell=False)
-    os.system("renice -n 20 %s" % (mid1.pid,))
-    mid1.wait()
+    # mid1 = subprocess.Popen(
+    #     ["ffmpeg", "-i", home + "/vids/vid_" + screen_num + ".mp4", "-s", "hd720",
+    #      home + "/vids/" + record_name + "mid_screen.mp4"], shell=False)
+    # os.system("renice -n 20 %s" % (mid1.pid,))
+    # mid1.wait()
 
-    mid2 = subprocess.Popen(
-        ["ffmpeg", "-i", home + "/vids/vid_" + cam_num + ".mp4", "-s", "hd720",
-         home + "/vids/" + record_name + "mid_cam.mp4"], shell=False)
-    os.system("renice -n 20 %s" % (mid2.pid,))
-    mid2.wait()
+    # mid2 = subprocess.Popen(
+    #     ["ffmpeg", "-i", home + "/vids/vid_" + cam_num + ".mp4", "-s", "hd720",
+    #      home + "/vids/" + record_name + "mid_cam.mp4"], shell=False)
+    # os.system("renice -n 20 %s" % (mid2.pid,))
+    # mid2.wait()
 
-    crop1 = subprocess.Popen(
-        ["ffmpeg", "-i", home + "/vids/" + record_name + "mid_cam.mp4", "-filter:v", "crop=640:720:40:0",
-         home + "/vids/" + record_name + "cropped_cam.mp4"], shell=False)
+    # crop1 = subprocess.Popen(
+    #     ["ffmpeg", "-i", home + "/vids/" + record_name + "mid_cam.mp4", "-filter:v", "crop=640:720:40:0",
+    #      home + "/vids/" + record_name + "cropped_cam.mp4"], shell=False)
 
-    os.system("renice -n 20 %s" % (crop1.pid,))
-    crop1.wait()
+    # os.system("renice -n 20 %s" % (crop1.pid,))
+    # crop1.wait()
 
-    second = subprocess.Popen(
-        ["ffmpeg", "-i", home + "/vids/" + record_name + "cropped_cam.mp4", "-i", home + "/vids/" +
-         record_name + "mid_screen.mp4", "-filter_complex", "hstack=inputs=2", home + "/vids/vid_" +
-         record_name + "merged.mp4"], shell=False)
-    os.system("renice -n 20 %s" % (second.pid,))
-    second.wait()
+    # second = subprocess.Popen(
+    #     ["ffmpeg", "-i", home + "/vids/" + record_name + "cropped_cam.mp4", "-i", home + "/vids/" +
+    #      record_name + "mid_screen.mp4", "-filter_complex", "hstack=inputs=2", home + "/vids/vid_" +
+    #      record_name + "merged.mp4"], shell=False)
+    # os.system("renice -n 20 %s" % (second.pid,))
+    # second.wait()
 
     res = ""
     if os.path.exists(home + "/vids/sound_" + record_name + ".aac"):
