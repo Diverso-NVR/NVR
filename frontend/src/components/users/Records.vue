1ab143f34d709c3319d7347254921622effec50a
Иван Мурзин <iemurzin@miem.hse.ru>
2020-08-29 13:22:39 +0000

    Add new file

---
+++ frontend/src/components/users/Records.vue
@@ -0,0 +1,16 @@
+<template> 
+    <div>
+<iframe v-for="record in records" :key="link" width="640" height="480" v-bind:src="record">
+</iframe>
+</div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: mapState({
+    records: state => state.records,
+  })
+};
+</script>
+
