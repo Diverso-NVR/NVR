7933a13bfecf32e4de4d9136e49188de710d0eb1
Иван Мурзин <iemurzin@miem.hse.ru>
2020-09-08 20:22:20 +0000

    Update Records.vue

--- frontend/src/components/users/Records.vue
+++ frontend/src/components/users/Records.vue
@@ -1,8 +1,8 @@
 <template> 
-    <div>
-<iframe v-for="record in records" :key="link" width="640" height="480" v-bind:src="record">
-</iframe>
-</div>
+  <div>
+    <iframe v-for="record in records" :key="record.drive_file_url" width="640" height="480" v-bind:src="record.drive_file_url">
+    </iframe>
+  </div>
 </template>
 
 <script>
